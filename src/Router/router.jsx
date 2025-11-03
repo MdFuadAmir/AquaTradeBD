8f0ea48c182d9635d19dc755438d7fcad0df0185
Md Fuad Amir <mdfuadamir@gmail.com>
2025-11-03 16:55:00 +0600

    added froject setup

---
+++ src/Router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<div>HOme</div>
+    }
+])
+
+export default router;
